86a5d2d4e97101b557ebd617402f8cda291e7d19
Akash16s <akashs1698@gmail.com>
2018-03-24 01:17:42 +0530

    final touch

--- app/src/main/java/com/akash/sky/scarnesdice/MainActivity.java
+++ app/src/main/java/com/akash/sky/scarnesdice/MainActivity.java
@@ -1,5 +1,6 @@
 package com.akash.sky.scarnesdice;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     Button roll,hold,reset;
     TextView yourscore,compscore;
     ImageView diceImg;
-    int overUserScore=0,userScore,overCompScore=0,compScore;
+    int overUserScore=0,userScore=0,overCompScore=0,compScore=0;
     private Random random = new Random();
 
     @Override
@@ -30,15 +31,102 @@ public class MainActivity extends AppCompatActivity {
 
     public void rollDice(View view){
         Integer ren = random.nextInt(6);
-        switch (ren) {
-            case 1: diceImg.setImageResource(R.drawable.dice1); break;
-            case 2: diceImg.setImageResource(R.drawable.dice2); break;
-            case 3: diceImg.setImageResource(R.drawable.dice3); break;
-            case 4: diceImg.setImageResource(R.drawable.dice4); break;
-            case 5: diceImg.setImageResource(R.drawable.dice5); break;
-            case 6: diceImg.setImageResource(R.drawable.dice6); break;
+            switch (ren) {
+                case 1:
+                    diceImg.setImageResource(R.drawable.dice1);
+                    overUserScore=overUserScore-userScore;
+                    yourscore.setText("Your Score : "+overUserScore);
+                    computerTurn();
+                    break;
+                case 2:
+                    diceImg.setImageResource(R.drawable.dice2);
+                    userScore = userScore + 2;
+                    overUserScore=overUserScore+2;
+                    yourscore.setText("Your Score : "+overUserScore);
+                    break;
+                case 3:
+                    diceImg.setImageResource(R.drawable.dice3);
+                    userScore = userScore + 3;
+                    overUserScore=overUserScore+3;
+                    yourscore.setText("Your Score : "+overUserScore);
+                    break;
+                case 4:
+                    diceImg.setImageResource(R.drawable.dice4);
+                    userScore = userScore + 4;
+                    overUserScore=overUserScore+4;
+                    yourscore.setText("Your Score : "+overUserScore);
+                    break;
+                case 5:
+                    diceImg.setImageResource(R.drawable.dice5);
+                    userScore = userScore + 5;
+                    overUserScore=overUserScore+5;
+                    yourscore.setText("Your Score : "+overUserScore);
+                    break;
+                case 6:
+                    diceImg.setImageResource(R.drawable.dice6);
+                    userScore = userScore + 6;
+                    overUserScore=overUserScore+userScore;
+                    yourscore.setText("Your Score : "+overUserScore);
+                    break;
+
+            }
+
+
+    }
+
+
+    public void computerTurn(){
+        Handler h2 = new Handler();
+        hold.setEnabled(false);
+        roll.setEnabled(false);
+        Integer ren=0;
+        Integer chance=random.nextInt(5);
+        int i=0;
+        while(i<chance && ren!=1) {
+            ren = random.nextInt(7);
+            switch (ren) {
+                case 1:
+                    diceImg.setImageResource(R.drawable.dice1);
+                    overCompScore = overCompScore - compScore;
+                    compscore.setText("Computer Score : " + overCompScore);
+                    break;
+                case 2:
+                    diceImg.setImageResource(R.drawable.dice2);
+                    compScore = compScore + 2;
+                    overCompScore = overCompScore + 2;
+                    compscore.setText("Computer Score : " + overCompScore);
+                    break;
+                case 3:
+                    diceImg.setImageResource(R.drawable.dice3);
+                    compScore = compScore + 3;
+                    overCompScore = overCompScore + 3;
+                    compscore.setText("Computer Score : " + overCompScore);
+                    break;
+                case 4:
+                    diceImg.setImageResource(R.drawable.dice4);
+                    compScore = compScore + 4;
+                    overCompScore = overCompScore + 4;
+                    yourscore.setText("Computer Score : " + overCompScore);
+                    break;
+                case 5:
+                    diceImg.setImageResource(R.drawable.dice5);
+                    compScore = compScore + 5;
+                    overCompScore = overCompScore + 5;
+                    yourscore.setText("Computer Score : " + overCompScore);
+                    break;
+                case 6:
+                    diceImg.setImageResource(R.drawable.dice6);
+                    compScore = compScore + 6;
+                    overCompScore = overCompScore + userScore;
+                    yourscore.setText("Computer Score : " + overCompScore);
+                    break;
+            }
+            i++;
+
+            h2.postDelayed((Runnable) this,500);
 
         }
-
+        hold.setEnabled(true);
+        roll.setEnabled(true);
     }
 }
